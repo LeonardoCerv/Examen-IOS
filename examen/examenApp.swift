f6803284be79b89e669b371fcfff2021d3c35144
onyxdude <leocerva29@gmail.com>
2025-11-27 15:08:32 -0600

    Initial Commit

---
+++ examen/examenApp.swift
@@ -0,0 +1,17 @@
+//
+//  examenApp.swift
+//  examen
+//
+//  Created by Leonardo Cervantes on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct examenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ examenTests/examenTests.swift
@@ -0,0 +1,17 @@
+//
+//  examenTests.swift
+//  examenTests
+//
+//  Created by Leonardo Cervantes on 27/11/25.
+//
+
+import Testing
+@testable import examen
+
+struct examenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
